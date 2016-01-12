01418869fc6fa60c896436ee1965b1df0f02d678
chxuan <787280310@qq.com>
2016-01-12 21:12:27 +0800

    Update net-rcf

---
+++ net-rcf/include/RCFSubscriberImpl.hpp
@@ -0,0 +1,13 @@
+/* Copyright(C)
+* For free
+* All right reserved
+* 
+*/
+/**
+* @file RCFSubscriberImpl.hpp
+* @brief 订阅者实现类
+* @author highway-9, 787280310@qq.com
+* @version 1.1.0
+* @date 2016-01-12
+*/
+
---
+++ net-rcf/include/RCFSubscriberWrapper.hpp
@@ -0,0 +1,13 @@
+/* Copyright(C)
+* For free
+* All right reserved
+* 
+*/
+/**
+* @file RCFSubscriberWrapper.hpp
+* @brief 订阅者包装类
+* @author highway-9, 787280310@qq.com
+* @version 1.1.0
+* @date 2016-01-12
+*/
+
